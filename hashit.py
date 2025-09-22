c4eee5f7201d64d9ea0af6c8b0cac1cd6609b1b1
belly <67766715+cyberbelly@users.noreply.github.com>
2025-09-22 19:26:25 -0400

    Update hashit.py

--- hashit.py
+++ hashit.py
@@ -3,7 +3,6 @@
 import subprocess
 from sys import argv
 
-argv=['hashit/hashit.py']
 for x in argv:
     md5raw=subprocess.run(['md5sum',x],stdout=subprocess.PIPE)
     sh1raw= subprocess.run(['sha1sum',x],stdout=subprocess.PIPE)
